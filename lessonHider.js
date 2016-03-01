ae00712b059a7303c9159907533d3a26f375a944
Alex Larson <alarson85@gmail.com>
2016-03-01 17:35:31 -0600

    MOstly done, need help on last step

---
+++ lessonHider.js
@@ -0,0 +1,33 @@
+angular.module('directivePractice').directive('lessonHider', function(){
+	
+	return{
+		templateUrl: 'lessonHider.html',
+		restrict: 'E' ,
+		scope: {
+			lesson: '='
+		} ,
+		controller: function($scope, lessonService){
+			$scope.getSchedule =lessonService.getSchedule();
+		} ,
+		link: function(scope, element, attrs){
+			scope.getSchedule.then(function(response){
+				scope.schedule = response.data;
+
+				scope.schedule.forEach(function( scheduleDay ) {
+					if (scheduleDay.lesson === scope.lesson) {
+						element.css('text-decoration', 'line-through');
+						scope.lessonDay = ['weekday']
+						return;
+					}
+				});
+		}),
+		scope: {
+			dayAlert: '&'
+		}
+
+
+}
+
+}
+
+});
