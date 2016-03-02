7a8cdafdadb2e3da871b5712f2977ebc0a5f691f
Alex Larson <alarson85@gmail.com>
2016-03-02 10:01:00 -0600

    debugged and working

--- lessonHider.js
+++ lessonHider.js
@@ -4,30 +4,26 @@ angular.module('directivePractice').directive('lessonHider', function(){
 		templateUrl: 'lessonHider.html',
 		restrict: 'E' ,
 		scope: {
-			lesson: '='
+			lesson: '=' ,
+			dayAlert: '&'
 		} ,
+
 		controller: function($scope, lessonService){
 			$scope.getSchedule =lessonService.getSchedule();
 		} ,
+
 		link: function(scope, element, attrs){
 			scope.getSchedule.then(function(response){
 				scope.schedule = response.data;
 
 				scope.schedule.forEach(function( scheduleDay ) {
 					if (scheduleDay.lesson === scope.lesson) {
-						element.css('text-decoration', 'line-through');
-						scope.lessonDay = ['weekday']
+						scope.lessonDay = scheduleDay.weekday;
 						return;
-					}
+						element.css('text-decoration', 'line-through');
+						}
 				});
-		}),
-		scope: {
-			dayAlert: '&'
+			})
 		}
-
-
-}
-
-}
-
+	};
 });
